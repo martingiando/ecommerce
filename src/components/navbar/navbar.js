b4fe1439a639eaf515b98f50373d7fdc32befe5a
Martin Giando <martingiando@gmail.com>
2020-11-04 01:48:16 -0300

    Commit prueba

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+
+const Menu = () =>{
+    return(
+        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">Game Masters</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Contacto</Nav.Link>
+            <NavDropdown title="Productos" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Todos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Mas comprados</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Ofertas</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Buscador" className="mr-sm-2" />
+            <Button variant="outline-success">Buscar</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    )}
+
+export default Menu
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Menu from './components/navbar/navbar'
+
+
+// import App from './App';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Menu />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
